84fc1fd3590615d692324a3f1a2f528f42c187ea
km633 <km633@cornell.edu>
2021-06-01 16:40:36 -0400

    removed depreciated version

--- sim/SPI_pymtl/SPIStack/__init__.py
+++
@@ -1,9 +0,0 @@
-#=========================================================================
-# SPIMinion
-#=========================================================================
-
-from .SPIStack      import SPIStack
-from ..SPIMinion    import SPIMinion
-from ..SPIQueue     import SPIQueue
-
-
