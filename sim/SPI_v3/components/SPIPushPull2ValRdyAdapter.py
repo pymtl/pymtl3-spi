d70af6e7ba7ce7977ac8b84b902a2d82620c297a
ki88 <ki88@cornell.edu>
2021-12-04 19:58:37 -0500

    Added message bitstruct and testing

--- sim/SPI_v3/components/SPIPushPull2ValRdyAdapter.py
+++ sim/SPI_v3/components/SPIPushPull2ValRdyAdapter.py
@@ -7,42 +7,33 @@ An Adapter that converts push/pull interface from SPI to val/rdy interfaces.
 Author : Kyle Infantino
   Date : Nov 30, 2021
 
-Message format
-
-pull_msg
-  nbits-1   nbits-2   nbits-2:0
-------------------------------
-| val_read | val_write | msg |
-------------------------------
-
-push_msg
-  nbits-1   nbits-2   nbits-2:0
-----------------------------
-| val_msg | flow_bit | msg |
-----------------------------
 '''
 from pymtl3 import *
 from pymtl3.stdlib.queues.queues import NormalQueueRTL
 from ..interfaces import PushInIfc, PullOutIfc
 
-# @bitstruct
-# class MisoMsg(nbits):
-#   val: Bits32
-#   space: Bits32
-#   msg: 
+def mk_miso_msg(nbits):
+  @bitstruct
+  class MisoMsg:
+    val: Bits1
+    spc: Bits1
+    data: mk_bits(nbits-2)
+  return MisoMsg
 
-# @bitstruct
-# class MosiMsg(nbits):
-#   val_wrt: Bits32
-#   val_rd: Bits32
-#   msg: 
+def mk_mosi_msg(nbits):
+  @bitstruct
+  class MosiMsg:
+    val_wrt: Bits1
+    val_rd: Bits1
+    data: mk_bits(nbits-2)
+  return MosiMsg
 
 class SPIPushPull2ValRdyAdapter( Component ):
 
   def construct( s, nbits=32, num_entries=2 ):
     s.nbits = nbits
-    s.pull = PushInIfc( nbits ) #interfaces from perspective of SPIMinion
-    s.push = PullOutIfc( nbits )
+    s.pull = PushInIfc( mk_mosi_msg(nbits) ) #interfaces from perspective of SPIMinion
+    s.push = PullOutIfc( mk_miso_msg(nbits) )
 
     s.recv_val = InPort()
     s.recv_rdy = OutPort()
@@ -51,13 +42,13 @@ class SPIPushPull2ValRdyAdapter( Component ):
     s.send_rdy = InPort()
     s.send_msg = OutPort(nbits-2)
 
-    s.mosiqueue = mc = NormalQueueRTL(nbits-2, num_entries) #TODO: change to have same cycle enq/deq
+    s.mosiqueue = mc = NormalQueueRTL(nbits-2, num_entries)
     mc.deq.rdy //= s.send_val
     mc.deq.ret //= s.send_msg
     mc.deq.en //= lambda: s.send_val & s.send_rdy
     s.mc_enq_en = Wire(1)
     mc.enq.en //= s.mc_enq_en
-    mc.enq.msg //= s.pull.msg[0:nbits-2]
+    mc.enq.msg //= s.pull.msg.data
 
 
     s.misoqueue = cm = NormalQueueRTL(nbits-2, num_entries)
@@ -67,6 +58,8 @@ class SPIPushPull2ValRdyAdapter( Component ):
 
     s.cm_deq_en = Wire(1)
     cm.deq.en //= s.cm_deq_en
+
+    s.push.msg.data //= cm.deq.ret
     
     s.flow_bit = Wire(1)
     
@@ -75,13 +68,14 @@ class SPIPushPull2ValRdyAdapter( Component ):
     @update
     def comb_block():
       s.open_entries @= mc.count < num_entries-1
-      s.mc_enq_en @= mc.enq.rdy & s.pull.msg[nbits-2] & s.pull.en
+      s.mc_enq_en @= mc.enq.rdy & s.pull.msg.val_wrt & s.pull.en
       s.flow_bit @= mc.enq.rdy & (~mc.enq.en | s.open_entries)
-      s.cm_deq_en @= cm.deq.rdy & s.pull.msg[nbits-1] & s.push.en & s.pull.en
-      s.push.msg @= concat( s.cm_deq_en, s.flow_bit, cm.deq.ret )
+      s.cm_deq_en @= cm.deq.rdy & s.pull.msg.val_rd & s.push.en & s.pull.en
+      s.push.msg.val @= s.cm_deq_en
+      s.push.msg.spc @= s.flow_bit
       
 
   def line_trace( s ):
     mc = s.mosiqueue
     cm = s.misoqueue
-    return f"mc_deq_en {mc.deq.en} mc_enq_en {mc.enq.en} mc_enq_rdy {mc.enq.rdy} mc_enq_msg {mc.enq.msg} cm_enq_en {cm.enq.en} cm_deq_en {cm.deq.en} cm_deq_rdy {cm.deq.rdy} mc_count {mc.count} cm_count {cm.count} val_write {s.pull.msg[s.nbits-2]} pull_en {s.pull.en} pull_msg {s.pull.msg}"
+    return f"mc_enq_en {mc.enq.en} mc_enq_rdy {mc.enq.rdy} mc_deq_en {mc.deq.en} cm_enq_en {cm.enq.en} cm_deq_en {cm.deq.en} cm_deq_rdy {cm.deq.rdy} mc_count {mc.count} cm_count {cm.count}"
--- sim/SPI_v3/test/SPIPushPull2ValRdyAdapter_test.py
+++ sim/SPI_v3/test/SPIPushPull2ValRdyAdapter_test.py
@@ -18,38 +18,40 @@ def test_basic( cmdline_opts ):
 
   dut.push.en @= 0
   dut.pull.en @= 0
-  dut.pull.msg @= 0
+  dut.pull.msg.val_rd @= 0
+  dut.pull.msg.val_wrt @= 0
+  dut.pull.msg.data @= 0
   dut.recv_val @= 0
   dut.recv_msg @= 0
   dut.send_rdy @= 0
   dut.sim_reset()
 
   # Test vectors
-  #      push_en, push_msg, pull_en, pull_msg, recv_val, recv_rdy, recv_msg, send_val, send_rdy, send_msg
-  t( dut,  0,      0x4,       0,     0x0,         0,        1,      0x0,       0,         0,      '?') #init
-  t( dut,  0,      0x4,       0,     0xF,         0,        1,      0x3,       0,         0,      '?') #add msg no write
-  t( dut,  0,      0x0,       1,     0x7,         0,        1,      0x0,       0,         0,      '?') #MOSI msg
-  t( dut,  0,      0x0,       0,     0x0,         0,        1,      0x0,       1,         0,      0x3) #send msg
-  t( dut,  0,      0x0,       0,     0x0,         0,        1,      0x0,       1,         1,      0x3) #accept send msg
-  
-  t( dut,  0,      0x4,       0,     0x0,         0,        1,      0x0,       0,         1,      '?') #empty
-  t( dut,  0,      0x4,       0,     0x0,         1,        1,      0x3,       0,         1,      '?') #recv msg
-  t( dut,  0,      0x7,       0,     0x0,         0,        0,      0x0,       0,         1,      '?') #MISO msg not accepted
-  t( dut,  1,      0xF,       1,     0x8,         0,        0,      0x0,       0,         1,      '?') #accept MISO msg
-  
-  t( dut,  0,      0x7,       0,     0x0,         0,        1,      0x0,       0,         1,      '?') #empty
-  t( dut,  0,      0x3,       1,     0x6,         1,        1,      0x1,       0,         1,      '?') #write both queues
-  t( dut,  1,      0x9,       1,     0x8,         0,        0,      0x0,       1,         1,      0x2) #read both queues
-  
-  t( dut,  1,      0x1,       1,     0x5,         1,        1,      0x2,       0,         0,      '?') #multiple writes both queues
-  t( dut,  0,      0x2,       1,     0x2,         1,        0,      0x1,       1,         0,      0x1) #invalid write 
-  t( dut,  1,      0xA,       1,     0x8,         0,        0,      0x0,       1,         1,      0x1) #read both queues - 0xA->0xE w/queue update
+  #      push_en, push_val, push_spc, push_data, pull_en, pull_val_wrt, pull_val_rd, pull_data, recv_val, recv_rdy, recv_msg, send_val, send_rdy, send_msg 
+  t( dut,  0,       0,         1,         '?',       0,         0,             0,      0x0,         0,        1,      0x0,       0,         0,      '?') #init
+  t( dut,  0,       0,         1,         '?',       0,         1,             1,      0x3,         0,        1,      0x3,       0,         0,      '?') #add msg no write
+  t( dut,  0,       0,         0,         '?',       1,         1,             0,      0x3,         0,        1,      0x0,       0,         0,      '?') #MOSI msg
+  t( dut,  0,       0,         0,         '?',       0,         0,             0,      0x0,         0,        1,      0x0,       1,         0,      0x3) #send msg
+  t( dut,  0,       0,         0,         '?',       0,         0,             0,      0x0,         0,        1,      0x0,       1,         1,      0x3) #accept send msg
+                                                                                                                                                             
+  t( dut,  0,       0,         1,         '?',       0,         0,             0,      0x0,         0,        1,      0x0,       0,         1,      '?') #empty
+  t( dut,  0,       0,         1,         '?',       0,         0,             0,      0x0,         1,        1,      0x3,       0,         1,      '?') #recv msg
+  t( dut,  0,       0,         1,         '?',       0,         0,             0,      0x0,         0,        0,      0x0,       0,         1,      '?') #MISO msg not accepted
+  t( dut,  1,       1,         1,         0x3,       1,         0,             1,      0x0,         0,        0,      0x0,       0,         1,      '?') #accept MISO msg
+                                                                                                                                                               
+  t( dut,  0,       0,         1,         '?',       0,         0,             0,      0x0,         0,        1,      0x0,       0,         1,      '?') #empty
+  t( dut,  0,       0,         0,         '?',       1,         1,             0,      0x2,         1,        1,      0x1,       0,         1,      '?') #write both queues
+  t( dut,  1,       1,         0,         0x1,       1,         0,             1,      0x0,         0,        0,      0x0,       1,         1,      0x2) #read both queues
+                                                                                                                                                               
+  t( dut,  1,       0,         0,         '?',       1,         1,             0,      0x1,         1,        1,      0x2,       0,         0,      '?') #multiple writes both queues
+  t( dut,  0,       0,         0,         '?',       1,         0,             0,      0x2,         1,        0,      0x1,       1,         0,      0x1) #invalid write 
+  t( dut,  1,       1,         0,         0x2,       1,         0,             1,      0x0,         0,        0,      0x0,       1,         1,      0x1) #read both queues - 0xA->0xE w/queue update
 
-  #realistic operation
-  t( dut,  1,      0x6,       1,     0x2,         0,        1,      0x0,       0,         0,      '?') #empty
-  t( dut,  1,      0x2,       1,     0xD,         1,        1,      0x3,       0,         0,      '?') #write both
-  t( dut,  1,      0x3,       1,     0x0,         0,        0,      0x0,       1,         1,      0x1) #read mosi
-  t( dut,  1,      0xB,       1,     0xF,         0,        0,      0x0,       0,         0,      '?') #read miso write mosi
+  # #realistic operation
+  t( dut,  1,       0,         1,         '?',       1,         0,             0,      0x2,         0,        1,      0x0,       0,         0,      '?') #empty
+  t( dut,  1,       0,         0,         '?',       1,         1,             1,      0x1,         1,        1,      0x3,       0,         0,      '?') #write both
+  t( dut,  1,       0,         0,         '?',       1,         0,             0,      0x0,         0,        0,      0x0,       1,         1,      0x1) #read mosi
+  t( dut,  1,       1,         0,         0x3,       1,         1,             1,      0x3,         0,        0,      0x0,       0,         0,      '?') #read miso write mosi
 
 
 def test_queue_len2( cmdline_opts ):
@@ -60,60 +62,59 @@ def test_queue_len2( cmdline_opts ):
 
   dut.push.en @= 0
   dut.pull.en @= 0
-  dut.pull.msg @= 0
+  dut.pull.msg.val_rd @= 0
+  dut.pull.msg.val_wrt @= 0
+  dut.pull.msg.data @= 0
   dut.recv_val @= 0
   dut.recv_msg @= 0
   dut.send_rdy @= 0
   dut.sim_reset()
+  
+# Test vectors
+#       push_en, push_val, push_spc, push_data, pull_en, pull_val_wrt, pull_val_rd, pull_data, recv_val, recv_rdy, recv_msg, send_val, send_rdy, send_msg 
+  t( dut,  0,        0,        1,       '?',       0,         0,            0,         0x0,         0,        1,      0x0,       0,         0,      '?') #init
+  t( dut,  0,        0,        1,       '?',       1,         1,            0,         0x3,         0,        1,      0x0,       0,         0,      '?') #MOSI msg
+  t( dut,  0,        0,        1,       '?',       0,         0,            0,         0x0,         0,        1,      0x0,       1,         1,      0x3) #send msg
+  
+  t( dut,  0,        0,        1,       '?',       0,         0,            0,         0x0,         1,        1,      0x3,       0,         1,      '?') #recv msg
+  t( dut,  1,        1,        1,       0x3,       1,         0,            1,         0x0,         0,        1,      0x0,       0,         1,      '?') #accept MISO msg
+  
+  t( dut,  0,        0,        1,       '?',       1,         1,            0,         0x2,         1,        1,      0x1,       0,         1,      '?') #write both queues
+  t( dut,  1,        1,        1,       0x1,       1,         0,            1,         0x0,         0,        1,      0x0,       1,         1,      0x2) #read both queues
+  
+  t( dut,  1,        0,        1,       '?',       1,         1,            0,         0x1,         1,        1,      0x2,       0,         0,      '?') #write both queues
+  t( dut,  1,        0,        0,       '?',       1,         1,            0,         0x2,         1,        1,      0x1,       1,         0,      '?') #write both queues
+  t( dut,  1,        1,        1,       0x2,       1,         0,            1,         0x0,         0,        0,      0x0,       1,         1,      0x1) #read both queues
+  t( dut,  1,        1,        1,       0x1,       1,         0,            1,         0x0,         0,        0,      0x0,       1,         1,      0x2) #read both queues
 
-  # Test vectors
-  #      push_en, push_msg, pull_en, pull_msg, recv_val, recv_rdy, recv_msg, send_val, send_rdy, send_msg
-  t( dut,  0,      0x4,       0,     0x0,         0,        1,      0x0,       0,         0,      '?') #init
-  t( dut,  0,      0x4,       1,     0x7,         0,        1,      0x0,       0,         0,      '?') #MOSI msg
-  t( dut,  0,      0x4,       0,     0x0,         0,        1,      0x0,       1,         1,      0x3) #accept send msg
-  
-  t( dut,  0,      0x4,       0,     0x0,         1,        1,      0x3,       0,         1,      '?') #recv msg
-  t( dut,  1,      0xF,       1,     0x8,         0,        1,      0x0,       0,         1,      '?') #accept MISO msg
-  
-  t( dut,  0,      0x4,       1,     0x6,         1,        1,      0x1,       0,         1,      '?') #write both queues
-  t( dut,  1,      0xD,       1,     0x8,         0,        1,      0x0,       1,         1,      0x2) #read both queues
-  
-  t( dut,  1,      0x7,       1,     0x5,         1,        1,      0x2,       0,         0,      '?') #write both queues
-  t( dut,  0,      0x2,       1,     0x6,         1,        1,      0x1,       1,         0,      '?') #write both queues
-  # t( dut,  1,      0xA,       1,     0x8,         0,        0,      0x0,       1,         1,      0x1) #read both queues - change w/queue update
-  # t( dut,  1,      0xA,       1,     0x8,         0,        0,      0x0,       1,         1,      0x1) #read both queues - change w/queue update
-
-  # #realistic operation
-  # t( dut,  1,      0x6,       1,     0x2,         0,        1,      0x0,       0,         0,      '?') #empty
-  # t( dut,  1,      0x2,       1,     0xD,         1,        1,      0x3,       0,         0,      '?') #write both
-  # t( dut,  1,      0x3,       1,     0x0,         0,        0,      0x0,       1,         1,      0x1) #read mosi
-  # t( dut,  1,      0xB,       1,     0xF,         0,        0,      0x0,       0,         0,      '?') #read miso write mosi
+  t( dut,  1,        0,        1,       '?',       1,         1,            0,         0x1,         1,        1,      0x1,       0,         0,      '?') #write both queues
+  t( dut,  1,        0,        0,       '?',       1,         1,            0,         0x2,         1,        1,      0x2,       1,         0,      '?') #write both queues
+  t( dut,  1,        1,        1,       0x1,       1,         0,            1,         0x0,         0,        0,      0x0,       1,         1,      0x1) #read both queues
+  t( dut,  1,        0,        0,       '?',       1,         1,            0,         0x3,         1,        1,      0x3,       1,         0,      '?') #write both queues
+  t( dut,  1,        1,        1,       0x2,       1,         0,            1,         0x0,         0,        0,      0x0,       1,         1,      0x2) #read both queues
+  t( dut,  1,        1,        1,       0x3,       1,         0,            1,         0x0,         0,        0,      0x0,       1,         1,      0x3) #read both queues
 
 
 # Helper function
-def t( dut, push_en, push_msg, pull_en, pull_msg, recv_val, recv_rdy, recv_msg, send_val, send_rdy, send_msg ):
+def t( dut, push_en, push_val, push_spc, push_data, pull_en, pull_val_wrt, pull_val_rd, pull_data, recv_val, recv_rdy, recv_msg, send_val, send_rdy, send_msg ):
 
   # Write input value to input port
   dut.push.en @= push_en
   dut.pull.en @= pull_en
-  dut.pull.msg @= pull_msg
+  dut.pull.msg.val_wrt @= pull_val_wrt
+  dut.pull.msg.val_rd @= pull_val_rd
+  dut.pull.msg.data @= pull_data
+
   dut.recv_val @= recv_val
   dut.recv_msg @= recv_msg
   dut.send_rdy @= send_rdy
-
   dut.sim_eval_combinational()
 
 
-  print(f"mc_deq_en {dut.mosiqueue.deq.en} mc_enq_en {dut.mosiqueue.enq.en} mc_enq_rdy {dut.mosiqueue.enq.rdy} mc_enq_msg {dut.mosiqueue.enq.msg} cm_enq_en {dut.misoqueue.enq.en} cm_deq_en {dut.misoqueue.deq.en} cm_deq_rdy {dut.misoqueue.deq.rdy} mc_cnt {dut.mosiqueue.count} cm_cnt {dut.misoqueue.count} val_wrt {dut.pull.msg[dut.nbits-2]} pull_en {dut.pull.en} pull_msg {dut.pull.msg} push_en {dut.push.en} val_res {dut.push.msg[dut.nbits-1]} push_msg {dut.push.msg} cm_deq_msg {dut.misoqueue.deq.ret} flw_bit {dut.flow_bit} opn_ents {dut.open_entries}")
+  print(f"mc_deq_en {dut.mosiqueue.deq.en} mc_enq_en {dut.mosiqueue.enq.en} mc_enq_rdy {dut.mosiqueue.enq.rdy} mc_enq_msg {dut.mosiqueue.enq.msg} cm_enq_en {dut.misoqueue.enq.en} cm_deq_en {dut.misoqueue.deq.en} cm_deq_rdy {dut.misoqueue.deq.rdy} mc_count {dut.mosiqueue.count} cm_count {dut.misoqueue.count} val_write {dut.pull.msg.val_wrt} pull_en {dut.pull.en} pull_msg {dut.pull.msg.data} flow_bit {dut.flow_bit}")
 
-  if push_msg != '?':
-    assert dut.push.msg == push_msg
-
-  if recv_rdy != '?':
-    assert dut.recv_rdy == recv_rdy
-
-  if send_val != '?':
-    assert dut.send_val == send_val
+  if push_data != '?':
+    assert dut.push.msg.data == push_data
 
   if send_msg != '?':
     assert dut.send_msg == send_msg
