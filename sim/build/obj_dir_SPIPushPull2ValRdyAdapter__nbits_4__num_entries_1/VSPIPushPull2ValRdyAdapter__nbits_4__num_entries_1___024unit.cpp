0a868161d7234c2c91fecbd1fa5176a67c587e4b
ki88 <ki88@cornell.edu>
2021-12-04 21:27:46 -0500

    removing build

--- sim/build/obj_dir_SPIPushPull2ValRdyAdapter__nbits_4__num_entries_1/VSPIPushPull2ValRdyAdapter__nbits_4__num_entries_1___024unit.cpp
+++
@@ -1,8 +0,0 @@
-// Verilated -*- C++ -*-
-// DESCRIPTION: Verilator output: Design implementation internals
-// See VSPIPushPull2ValRdyAdapter__nbits_4__num_entries_1.h for the primary calling header
-
-#include "VSPIPushPull2ValRdyAdapter__nbits_4__num_entries_1___024unit.h"
-#include "VSPIPushPull2ValRdyAdapter__nbits_4__num_entries_1__Syms.h"
-
-//==========
