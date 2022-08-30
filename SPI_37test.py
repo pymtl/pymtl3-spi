994f7797fd7f14773724111c618333ac57798507
Jack Brzozowski <jbrzozo24@github.com>
2022-08-30 09:12:31 -0400

    Updated test to use concat prints

--- SPI_37test.py
+++ SPI_37test.py
@@ -1,5 +1,5 @@
 from spidriver import SPIDriver
-port = '/dev/tty.usbserial-DO02C6P1'
+port = 'COM3'
 s = SPIDriver(port)
 
 # # read no write
@@ -21,10 +21,20 @@ s = SPIDriver(port)
 # print(resps) # if clear, result should be three 0x4000, followed by 0x0000
 
 # write and read
-for j in range(256):
+for j in range(3):
     s.sel()
-    msg = [0x19, j, j, j, j] # val read no write
+    msg = [0x19, j, 0, 20, j] # val read no write
     resps = list(s.writeread(msg))
-    resps[0] = hex(resps[0])
+    concat = ""
+    for i in range(len(resps)):
+        # concat += str(hex(resps[i]))[2:]
+        concat += str(format(resps[i], '#04x'))[2:]
+    concat = hex(int('0x'+ concat, 16) // 8)
+    resps[0] = concat[:4]
+    resps[1] = int(concat[4:6], 16)
+    resps[2] = int(concat[6:8], 16)
+    resps[3] = int(concat[8:10], 16)
+    resps[4] = int(concat[10:12], 16)
+
     s.unsel()
     print(resps) # if clear, result should be three 0x4000, followed by 0x0000
